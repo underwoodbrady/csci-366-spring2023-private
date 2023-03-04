f8ba709a65ebff7981c3533b2594002f0ceea695
Brady Underwood <underwood.brady@gmail.com>
2023-03-03 23:40:15 -0700

    assembly

--- assembly/examples/countdown.asm
+++ assembly/examples/countdown.asm
@@ -1,12 +1,15 @@
 .text
 
-li $t0, 10
+li $t0, 11 #end
+li $t1, 1 #start
+li $t2, 1 #start
+
 loop:
-  li $v0, 1 # print int
-  move $a0, $t0
-  syscall
-  subi $t0, $t0, 1
-  bgt $t0, $zero, loop
+  beq $t1, $t0, exitloop
+  move $a0, $t1
   
-li $v0, 10 # exit
-syscall
+  li $v0, 1 # print int
+  syscall
+  add $t1, $t1, $t2
+  j loop
+  exitloop:
--- assembly/homework/homework.asm
+++ assembly/homework/homework.asm
@@ -6,6 +6,18 @@
 // Please use a jump in your solution
 // Please use the OUT instruction to print values
 
+        LDA ONE
+        SUB ELEVEN 
+LOOP    BRZ EXIT  
+        ADD ELEVEN 
+        OUT
+        ADD ONE
+        SUB ELEVEN 
+        BRA LOOP
+EXIT    HLT
+ONE     DAT 1
+ELEVEN  DAT 11
+
 //==========================================================================================
 // PROBLEM 2: Counting Up From 1 to 10 in MIPS Assembly
 //==========================================================================================
@@ -13,3 +25,19 @@
 // You can develop your code via the MARS emulator and then paste it in here
 // Please use a jump in your solution
 // Please use sys-calls to output the value
+
+.text
+
+li $t0, 11 #end
+li $t1, 1 #start
+li $t2, 1 #start
+
+loop:
+  beq $t1, $t0, exitloop
+  move $a0, $t1
+  
+  li $v0, 1 # print int
+  syscall
+  add $t1, $t1, $t2
+  j loop
+  exitloop:
